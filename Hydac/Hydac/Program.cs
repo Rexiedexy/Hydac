f0f1b0775ab453f535078660ca0d7630c61a657b
Martin Johnsen <MartinJohnsen89@gmail.com>
2025-09-24 14:21:04 +0200

    Main menu

--- Hydac/Hydac/Program.cs
+++ Hydac/Hydac/Program.cs
@@ -4,6 +4,30 @@
     {
         static void Main(string[] args)
         {
+            string valgMuligheder;
+
+            do
+            {  
+                Console.Clear();
+                Console.WriteLine("Velkommen! Vælg hvad der er relevant for dig\n");
+                Console.WriteLine(" 1. Personale");
+                Console.WriteLine(" 2. Gæster");
+                Console.WriteLine(" 3. Booking af mødelokale");
+                Console.WriteLine(" 4. Administrator");
+
+                valgMuligheder = Console.ReadKey();
+
+                switch (valgMuligheder)
+                {
+                    case "1": 
+                    
+                    case "2": 
+
+                    case "3": 
+
+                    case "4": 
+                }
+             }    
             Console.WriteLine("Hello, World!");
         }
     }
