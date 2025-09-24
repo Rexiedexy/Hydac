92b36ea279b78c5dfc1ae5532bed417ee6f7a5b6
Asbjørn Andersen <andersenasbjornmeyer@gmail.com>
2025-09-24 13:11:35 +0200

    start

--- Hydac/Hydac/Guest.cs
+++ Hydac/Hydac/Guest.cs
@@ -8,5 +8,20 @@ namespace Hydac
 {
     public class Guest
     {
+        private List<string> guestList = new List<string>();
+        public void AddGuest(string name) => guestList.Add(name);
+        public void ShowGuests()
+        {
+            if(guestList.Count == 0)
+            {
+                Console.WriteLine("No guests available.");
+                return;
+            }
+            else
+                foreach (var guest in guestList)
+                {
+                    Console.WriteLine(guest);
+                }
+        }
     }
 }
