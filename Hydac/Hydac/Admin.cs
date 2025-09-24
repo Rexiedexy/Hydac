ee894b4d017240992907c9a122b3580ccf5c5f82
Rexiedexy <jlundgren07@gmail.com>
2025-09-24 13:30:21 +0200

    Admin

--- Hydac/Hydac/Admin.cs
+++ Hydac/Hydac/Admin.cs
@@ -8,5 +8,17 @@ namespace Hydac
 {
     public class Admin
     {
+        private string[] name = {"Børge", "Jacob", };
+        private List<string> adminMembers = new List<string>();
+        private string[] adminId = {"A22", "A27"};
+
+        public Admin(string name, string adminId)
+        {
+            this.name = name;
+            this.adminId = adminId;
+        }
+        public string Name { get { return name; } }
+        public string AdminId { get { return adminId; } }
+
     }
 }
