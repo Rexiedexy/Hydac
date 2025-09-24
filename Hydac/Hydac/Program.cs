c889eede7ff46d63c5d2afcdad990e8493dceb4c
Martin Johnsen <MartinJohnsen89@gmail.com>
2025-09-24 13:15:10 +0200

    s

--- Hydac/Hydac/Program.cs
+++ Hydac/Hydac/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace Hydac
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hydac
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+   
+
         }
     }
 }
