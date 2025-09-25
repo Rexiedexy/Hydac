bc1d65dd612d53fb1ff8de94d21e052c8f4a90af
Martin Johnsen <MartinJohnsen89@gmail.com>
2025-09-25 13:09:37 +0200

    Tilføjet menu

--- Hydac/Hydac/Program.cs
+++ Hydac/Hydac/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            string valgMuligheder;
+            string options;
 
             do
             {
@@ -15,18 +15,31 @@
                 Console.WriteLine(" 3. Booking af mødelokale");
                 Console.WriteLine(" 4. Administrator");
 
-                valgMuligheder = Console.ReadLine();
+                options = Console.ReadLine();
 
-                switch (valgMuligheder)
+                switch (options)
                 {
-                    case "1": Staff; break;
+                    case "1":
+                        Console.Clear();
+                        Console.WriteLine("Indtast din personaleid");
 
-                    case "2": Guest; break;
+                        Console.WriteLine("Hvad er dit humør?\n");
+                        Console.WriteLine(" Tryk 1. for Grøn\n Tryk 2. for Gul\n Tryk 3. for Rød");
+                       
+                        Console.WriteLine("Du er nu regisreret, god arbejdslyst :)");
 
-                    case "3": MeetingRoom; break;
+                    case "2":
+                        Console.Clear();
+                        Console.WriteLine("Indtast dit tilsendte gæsteid og tag derefter plads i lobbyen");
+                        
+                    case "3": 
+                        Console.Clear();
+                        Console.WriteLine("Hvilket mødelokale kunne du tænke dig at booke?\n");
+                        Console.WriteLine(" 1. Small living room\n 2. Canteen\n 3. Aquarium\n 4. Small room\n 5. Big room");
 
-                    case "4": Admin; break;
-                }
+                    case "4":
+                        Console.Clear();
+                        Console.WriteLine(" 1. Tilføj personale\n 2. Slet personale\n 3. Tilføj gæst\n 4. Slet gæst");
             }
         }
     }
